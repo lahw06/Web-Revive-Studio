b08685752e746d6d8d7bd634c854504664c4a2de
lahw06 <lahw06@virginmedia.com>
2025-03-04 12:32:55 +0000

    Add files via upload

---
+++ resources/javascript/nav.js
@@ -0,0 +1,39 @@
+var CSbody = document.querySelector('body');
+const CSnavbarMenu = document.querySelector('#cs-navigation');
+const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
+
+CShamburgerMenu.addEventListener('click', function () {
+	CShamburgerMenu.classList.toggle('cs-active');
+	CSnavbarMenu.classList.toggle('cs-active');
+	CSbody.classList.toggle('cs-open');
+	ariaExpanded();
+});
+
+
+function ariaExpanded() {
+	const csUL = document.querySelector('#cs-expanded');
+	const csExpanded = csUL.getAttribute('aria-expanded');
+
+	if (csExpanded === 'false') {
+		csUL.setAttribute('aria-expanded', 'true');
+	} else {
+		csUL.setAttribute('aria-expanded', 'false');
+	}
+}
+
+
+const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+for (const item of dropDowns) {
+	const onClick = () => {
+		item.classList.toggle('cs-active');
+	};
+	item.addEventListener('click', onClick);
+}
+window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("cs-navigation");
+    if (window.scrollY > 0) {
+        navbar.classList.add("full-width"); 
+    } else {
+        navbar.classList.remove("full-width"); 
+    }
+});
